69e5bdeea8082d570b8a6491f14967a40668cc1a
Denis <denis30510@gmail.com>
2023-06-05 15:45:55 +0300

    added str, repr, dequeue and test

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -8,6 +8,8 @@ class TestNodeQueue(unittest.TestCase):
     def test_node(self):
         """Тест Node"""
         tests_node = Node('data1', None)
+        self.assertEqual(repr(tests_node), "Node('data1',None)")
+        self.assertEqual(str(tests_node), 'data1')
         first_item = Node('data2', tests_node)
         self.assertEqual(first_item.data, 'data2')
         self.assertEqual(first_item.next_node.data, 'data1')
@@ -17,6 +19,7 @@ class TestNodeQueue(unittest.TestCase):
         """Тест Queue"""
         queue = Queue()
         self.assertEqual(queue.all_data, '')
+        self.assertEqual(repr(queue), "Queue(None,None)")
         queue.enqueue('data1')
         queue.enqueue('data2')
         self.assertEqual(queue.head.data, 'data1')
@@ -24,3 +27,9 @@ class TestNodeQueue(unittest.TestCase):
         self.assertEqual(queue.tail.data, 'data2')
         queue.enqueue('data3')
         self.assertEqual(queue.tail.data, 'data3')
+
+        # Test dequeue
+        self.assertEqual(queue.dequeue(), 'data1')
+        queue.dequeue()
+        queue.dequeue()
+        self.assertIs(queue.dequeue(), None)
