4146e95908d0eaf1e7aa6c96060b44f5b909893c
Denis <denis30510@gmail.com>
2023-06-08 15:38:41 +0300

    added LinkList, Node and test

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,18 +1,33 @@
 class Node:
     """Класс для узла односвязного списка"""
-    pass
+    def __init__(self, data: dict, next_node=None) -> None:
+        self.data = data
+        self.next_node = next_node
 
 
 class LinkedList:
     """Класс для односвязного списка"""
+    head = None
+    tail = None
 
     def insert_beginning(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-        pass
+        node = Node(data, self.tail)
+        if self.head is None:
+            self.head = node
+            self.tail = node
+        else:
+            next_head = self.head
+            self.head = node
+            self.head.next_node = next_head
 
     def insert_at_end(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
-        pass
+        node = Node(data)
+        self.tail.next_node = node
+        self.tail = node
+
+
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
@@ -22,8 +37,8 @@ class LinkedList:
 
         ll_string = ''
         while node:
-            ll_string += f' {str(node.data)} ->'
+            ll_string += f'{str(node.data)} -> '
             node = node.next_node
 
-        ll_string += ' None'
+        ll_string += 'None'
         return ll_string
